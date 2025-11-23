3c2d9dae8ea58f81f9605a42c87b64d129a31bc7
nisalmissaka <wishwakanisal@gmail.com>
2025-11-23 11:58:35 +0530

    finel commit

--- src/main/java/edu/iect/controller/ProductController.java
+++ src/main/java/edu/iect/controller/ProductController.java
@@ -3,6 +3,7 @@ package edu.iect.controller;
 import edu.iect.dto.Product;
 import edu.iect.service.ProductService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,23 +11,21 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/product")
-@CrossOrigin
+@RequestMapping("product")
+@CrossOrigin(origins = "https://localhost:5174")
 public class ProductController {
 
-    private final ProductService service;
+     final ProductService service;
 
-    @PostMapping
+    @PostMapping("/add")
+
     public void addProduct(@RequestBody Product product){
         service.addProduct(product);
+
     }
     @GetMapping("/get-all")
     public List<Product>getAll(){
         return service.getAll();
-        }
-    @PostMapping("/update")
-    public void updateProduct(@RequestBody Product product){
-       service.updateProduct(product);
 
     }
 }
--- src/main/java/edu/iect/service/impl/ProductServiceImpl.java
+++ src/main/java/edu/iect/service/impl/ProductServiceImpl.java
@@ -1,18 +1,43 @@
 package edu.iect.service.impl;
 
 import edu.iect.dto.Product;
+import edu.iect.entity.ProductEntity;
+import edu.iect.repository.ProductRepository;
 import edu.iect.service.ProductService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private final ModelMapper modelMapper = new ModelMapper();
+
     @Override
     public void addProduct(Product product) {
-
+        ProductEntity entity = modelMapper.map(product, ProductEntity.class);
+        productRepository.save(entity);
     }
 
     @Override
     public List<Product> getAll() {
-        return List.of();
+        return productRepository.findAll()
+                .stream()
+                .map(entity -> modelMapper.map(entity, Product.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void updateProduct(Product product) {
+        // Update will work with save() if ID exists
+        ProductEntity entity = modelMapper.map(product, ProductEntity.class);
+        productRepository.save(entity);
     }
 }
+
